617bb5c698619085c05129ea9bc523d4e30935d8
Rafael Lima <rafalima07@gmail.com>
2020-10-28 22:12:33 +0000

    Remove all application code

--- app/src/test/java/bdd/automation/api/AppTest.java
+++ app/src/test/java/bdd/automation/api/AppTest.java
@@ -8,7 +8,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+        assertTrue(1 == 1);
     }
 }
