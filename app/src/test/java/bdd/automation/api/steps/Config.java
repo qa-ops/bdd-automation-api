f508660abcbf8feb6e857c734ed8437ca46338e9
Rafael Lima <rafalima07@gmail.com>
2020-11-14 10:45:30 +0000

    Add RestAssured default setup

---
+++ app/src/test/java/bdd/automation/api/steps/Config.java
@@ -0,0 +1,32 @@
+package bdd.automation.api.steps;
+
+import io.cucumber.java.Before;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+
+public class Config {
+
+    @Before
+    public void setup() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+        RestAssured.baseURI = "http://localhost:12345";
+        RestAssured.basePath = "/api";
+
+        RestAssured.requestSpecification = new RequestSpecBuilder().
+            addHeader("Authorization", getToken()).
+            setContentType(ContentType.JSON).
+            build();
+
+        RestAssured.responseSpecification = new ResponseSpecBuilder().
+            expectContentType(ContentType.JSON).
+            build();
+    }
+
+    private String getToken() {
+        return "grant access";
+    }
+
+}
