f4d2b7756cff3c175c599119fdb1eb56bbaa886c
Rafael Lima <rafalima07@gmail.com>
2020-12-18 13:47:59 +0000

    Add pet domain

---
+++ app/src/test/java/bdd/automation/api/steps/PetStepDefinitions.java
@@ -0,0 +1,17 @@
+package bdd.automation.api.steps;
+
+import bdd.automation.api.support.domain.Pet;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class PetStepDefinitions {
+    @Given("that I have pets available")
+    public void thatIHavePetsAvailable() throws JsonProcessingException {
+        Pet pet = Pet.builder().build();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(pet);
+        System.out.println(json);
+    }
+}
