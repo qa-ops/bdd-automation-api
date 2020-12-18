a8166e354859ae622cd54297f535022d504b8f11
Rafael Lima <rafalima07@gmail.com>
2020-12-18 10:58:45 +0000

    Add cucumber hooks

--- app/src/test/java/bdd/automation/api/steps/Config.java
+++ app/src/test/java/bdd/automation/api/steps/Config.java
@@ -1,5 +1,6 @@
 package bdd.automation.api.steps;
 
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
@@ -29,4 +30,39 @@ public class Config {
         return "grant access";
     }
 
+    @Before
+    public void doSomething() {
+        System.out.println("hook before");
+    }
+
+    @Before(value = "@first", order = 1)
+    public void doFirst() {
+        System.out.println("before first");
+    }
+
+    @Before(value = "@second", order = 2)
+    public void doSecond() {
+        System.out.println("before second");
+    }
+
+    @Before(value = "@third", order = 3)
+    public void doThird() {
+        System.out.println("before third");
+    }
+
+
+    @After(value = "@first", order = 3)
+    public void doLast() {
+        System.out.println("after first");
+    }
+
+    @After(value = "@second", order = 2)
+    public void doLast2() {
+        System.out.println("after second");
+    }
+
+    @After(value = "@third", order = 1)
+    public void doLast3() {
+        System.out.println("after third");
+    }
 }
