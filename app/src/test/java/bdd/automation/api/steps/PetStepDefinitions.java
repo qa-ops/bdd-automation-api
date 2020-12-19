71c43fad17c1511c6c46acbca922633693fc5ea7
Rafael Lima <rafalima07@gmail.com>
2020-12-19 22:46:56 +0000

    Adding the Pet domain class

--- app/src/test/java/bdd/automation/api/steps/PetStepDefinitions.java
+++ app/src/test/java/bdd/automation/api/steps/PetStepDefinitions.java
@@ -4,9 +4,11 @@ import bdd.automation.api.support.domain.Pet;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
+import io.cucumber.java.pt.Dado;
 
 public class PetStepDefinitions {
+
+    @Dado("que eu possua pets available")
     @Given("that I have pets available")
     public void thatIHavePetsAvailable() throws JsonProcessingException {
         Pet pet = Pet.builder().build();
