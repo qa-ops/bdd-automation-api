16cac774ee17885d33909665c7b6e36ff45a33bf
Rafael Lima <rafalima07@gmail.com>
2020-12-20 14:20:23 +0000

    Add PetApi class

--- app/src/test/java/bdd/automation/api/steps/PetStepDefinitions.java
+++ app/src/test/java/bdd/automation/api/steps/PetStepDefinitions.java
@@ -1,16 +1,37 @@
 package bdd.automation.api.steps;
 
+import bdd.automation.api.support.api.PetApi;
 import bdd.automation.api.support.domain.Pet;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 public class PetStepDefinitions {
+
+    private PetApi petApi;
+    private List<Pet> actualPets;
+
+    public PetStepDefinitions() {
+        petApi = new PetApi();
+    }
+
     @Given("that I have pets available")
-    public void thatIHavePetsAvailable() throws JsonProcessingException {
-        Pet pet = Pet.builder().build();
-        ObjectMapper objectMapper = new ObjectMapper();
-        String json = objectMapper.writeValueAsString(pet);
-        System.out.println(json);
+    public void thatIHavePetsAvailable() {}
+
+    @When("I search for all pets {word}")
+    public void iSearchForAllPetsAvailable(String status) {
+        actualPets = petApi.getPetsByStatus(status);
+    }
+
+    @Then("I receive a list of pets available")
+    public void iReceiveAListOfPetsAvailable() {
+        assertThat(actualPets, is(not(empty())));
     }
 }
