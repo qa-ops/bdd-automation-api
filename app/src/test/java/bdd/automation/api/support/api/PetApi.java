ebc4a20e2c0db08ee2ac4aac60450efe2fa8af21
Rafael Lima <rafalima07@gmail.com>
2020-12-20 17:37:29 +0000

    Adding example of rest assured old syntax

--- app/src/test/java/bdd/automation/api/support/api/PetApi.java
+++ app/src/test/java/bdd/automation/api/support/api/PetApi.java
@@ -1,6 +1,10 @@
 package bdd.automation.api.support.api;
 
 import bdd.automation.api.support.domain.Pet;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 import java.util.List;
 
@@ -10,13 +14,20 @@ public class PetApi {
 
     private static final String FIND_PETS_BY_STATUS_ENDPOINT = "v3/pet/findByStatus?status={status}";
 
+//    public List<Pet> getPetsByStatus(String status) {
+//        return given().
+//            pathParam("status", status).
+//        when().
+//            get(FIND_PETS_BY_STATUS_ENDPOINT).
+//        then().
+//            extract().body().jsonPath().getList("", Pet.class);
+//    }
+
     public List<Pet> getPetsByStatus(String status) {
-        return given().
-            pathParam("status", status).
-        when().
-            get(FIND_PETS_BY_STATUS_ENDPOINT).
-        then().
-            extract().body().jsonPath().getList("", Pet.class);
+        RequestSpecification httpRequest = RestAssured.given();
+        httpRequest.pathParam("status", status);
+        Response response = httpRequest.get(FIND_PETS_BY_STATUS_ENDPOINT);
+        return response.body().jsonPath().getList("", Pet.class);
     }
 
 }
