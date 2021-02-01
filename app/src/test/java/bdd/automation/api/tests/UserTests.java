7c75e50b69ca9254faecd0c98612afe3deb68e20
Rafael Lima <rafalima07@gmail.com>
2021-02-01 23:12:43 +0000

    Update user tests

--- app/src/test/java/bdd/automation/api/tests/UserTests.java
+++ app/src/test/java/bdd/automation/api/tests/UserTests.java
@@ -7,30 +7,26 @@ import org.junit.jupiter.api.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@DisplayName("Create new user tests")
 public class UserTests extends BaseTest {
 
-    @Nested
-    @DisplayName("Create new user tests")
-    class CreateNewUser {
+    UserApi userApi = new UserApi();
+    User expectedUser;
 
-        UserApi userApi = new UserApi();
-        User expectedUser;
+    @Test
+    @DisplayName("A user must be able to register to the system")
+    void mustBeAbleToCreateUser() {
+        expectedUser = User.builder().build();
 
-        @Test
-        @DisplayName("A user must be able to register to the system")
-        void mustBeAbleToCreateUser() {
-            expectedUser = User.builder().build();
+        userApi.createUser(expectedUser);
 
-            userApi.createUser(expectedUser);
+        String actualUsername = userApi.getUsername(expectedUser);
 
-            String actualUsername = userApi.getUsername(expectedUser);
+        assertThat(actualUsername, is(expectedUser.getUsername()));
+    }
 
-            assertThat(actualUsername, is(expectedUser.getUsername()));
-        }
-
-        @AfterEach
-        void deleteNewUser() {
-            userApi.deleteUser(expectedUser);
-        }
+    @AfterEach
+    void deleteNewUser() {
+        userApi.deleteUser(expectedUser);
     }
 }
--- app/src/test/resources/features/loja.feature
+++
@@ -1,8 +0,0 @@
-#language: pt
-Funcionalidade: Gerenciamento de pedidos
-
-  @DeleteExtraPets
-  Cenario: Cliente cria um pedido na loja
-    Dado que eu possua animal available
-    Quando faço o pedido desse animal
-    Então o pedido é aprovado
