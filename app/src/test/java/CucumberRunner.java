09fa76ca972ec0ff7ddf8c819318e32522c94d8d
Rafael Lima <rafalima07@gmail.com>
2021-03-08 23:14:03 +0000

    Add gradle properties

--- app/src/test/java/CucumberRunner.java
+++ app/src/test/java/CucumberRunner.java
@@ -5,7 +5,6 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         tags = "not @wip and not @quarantine",
-        plugin = {"pretty", "html:build/reports/feature.html"},
         features = {"src/test/resources/features"}
 )
 public class CucumberRunner {
